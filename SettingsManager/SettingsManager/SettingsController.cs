eddafd2b68e5018562f3152bbf1b4561d235007a
jairoguzman <jairo.guzman@carvajal.com>
2014-05-26 17:45:22 -0500

    Crea proyecto transversal de settings

---
+++ SettingsManager/SettingsManager/SettingsController.cs
@@ -0,0 +1,41 @@
+﻿using SettingsManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsManager
+{
+    public class SettingsController
+    {
+        #region public properties
+        public Dictionary<string, ModuleModel> ModulesParams { get; private set; }
+        #endregion
+
+        #region constructors
+        //standar read all modules
+        public SettingsController()
+        {
+            SettingsReader sr = new SettingsReader(System.Configuration.ConfigurationManager.AppSettings["SettingsConfig"]);
+            ModulesParams = sr.LoadAll();
+        }
+        //personalized read all modules
+        public SettingsController(string oSettingsConfigLocation)
+        {
+
+            SettingsReader sr = new SettingsReader(oSettingsConfigLocation);
+            ModulesParams = sr.LoadAll();
+        }
+        //personalized read one module
+        public SettingsController(string oSettingsConfigLocation, string ModuleName)
+        {
+
+            SettingsReader sr = new SettingsReader(oSettingsConfigLocation);
+            ModuleModel CurrentModule = sr.LoadModule(ModuleName);
+            ModulesParams = new Dictionary<string, ModuleModel>();
+            ModulesParams.Add(CurrentModule.Name, CurrentModule);
+        }
+        #endregion
+    }
+}
