be354e3eef87d9b171ee7cc0afcd9d42aaa38e4a
jairoguzman <jairo.guzman@carvajal.com>
2014-06-10 10:47:03 -0500

    Ajuste sobre la ejecucion de comandos con conexiones abiertas.

--- ADO/ADO/MYSQL/MySqlImplement.cs
+++ ADO/ADO/MYSQL/MySqlImplement.cs
@@ -51,10 +51,20 @@ namespace ADO.MYSQL
             switch (QueryParams.CommandExecutionType)
             {
                 case enumCommandExecutionType.NonQuery:
-                    oRetorno.NonQueryResult = CurrentCommand.ExecuteNonQuery();
+                    using (CurrentCommand.Connection)
+                    {
+                        CurrentCommand.Connection.Open();
+                        oRetorno.NonQueryResult = CurrentCommand.ExecuteNonQuery();
+                        CurrentCommand.Connection.Close();
+                    }
                     break;
                 case enumCommandExecutionType.Scalar:
-                    oRetorno.ScalarResult = CurrentCommand.ExecuteScalar();
+                    using (CurrentCommand.Connection)
+                    {
+                        CurrentCommand.Connection.Open();
+                        oRetorno.ScalarResult = CurrentCommand.ExecuteScalar();
+                        CurrentCommand.Connection.Close();
+                    }
                     break;
                 case enumCommandExecutionType.DataTable:
 
