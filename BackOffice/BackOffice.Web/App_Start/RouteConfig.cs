f43fd17c894f46ae9c2aea780fd14f29e007bf67
jairoguzman <jairo.guzman@carvajal.com>
2014-06-12 20:19:50 -0500

    Creacion backoffice

---
+++ BackOffice/BackOffice.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BackOffice.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            #region Route value dictionary by area
+            RouteValueDictionary rvdByArea = new RouteValueDictionary();
+            rvdByArea.Add("UseNamespaceFallback", false);
+            rvdByArea.Add("area", BackOffice.Web.Controllers.BaseController.AreaName);
+            rvdByArea.Add("Namespaces", new string[] { "BackOffice.Web.Areas." + BackOffice.Web.Controllers.BaseController.AreaName + ".*" });
+            #endregion
+
+            //ignore route
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //custom routes
+
+            //default route
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            ).DataTokens = rvdByArea;
+        }
+    }
+}
---
+++ BackOffice/BackOffice.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackOffice.Web.Controllers
+{
+    public partial class HomeController : BaseController
+    {
+        //Starup page project
+        public virtual ActionResult Index()
+        {
+            //validate user loggin
+            if (Auth.UserManager.LoginManager.UserIsLoggedIn)
+            {
+                //load autorization modules
+
+                //validate if user is autorized
+
+                //redirect to dashboard
+            }
+            return View();
+        }
+
+        public virtual ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
