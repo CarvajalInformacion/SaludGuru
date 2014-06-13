b643127513ad7d44fafea9c5d4d22afea2e7c205
jairoguzman <jairo.guzman@carvajal.com>
2014-06-13 08:31:13 -0500

    Agrega paquetes nuget de compilacion

---
+++ SaludGuru.Profile/Profile.Manager/DAL/Controller/ProfileDataController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile.Manager.DAL.Controller
+{
+    internal class ProfileDataController : Profile.Manager.Interfaces.IProfileData
+    {
+        private static Profile.Manager.Interfaces.IProfileData oInstance;
+        internal static Profile.Manager.Interfaces.IProfileData Instance
+        {
+            get
+            {
+                if (oInstance == null)
+                    oInstance = new ProfileDataController();
+                return oInstance;
+            }
+        }
+
+        #region Autorization
+        #endregion
+
+    }
+}
---
+++ SaludGuru.Profile/Profile.Manager/DAL/Controller/ProfileDataFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile.Manager.DAL.Controller
+{
+    internal class ProfileDataFactory
+    {
+        public Profile.Manager.Interfaces.IProfileData GetLogInstance()
+        {
+            Type typetoreturn = Type.GetType("Profile.Manager.DAL.MySQLDAO.Profile_MySqlDao,Profile.Manager");
+            Profile.Manager.Interfaces.IProfileData oRetorno = (Profile.Manager.Interfaces.IProfileData)Activator.CreateInstance(typetoreturn);
+            return oRetorno;
+        }
+    }
+}
