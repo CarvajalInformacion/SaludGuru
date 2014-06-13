9bd67dbb1f28575fd1d8c39d8bd041c4bcb9a455
jairoguzman <jairo.guzman@carvajal.com>
2014-06-13 08:51:56 -0500

    Crea administrador de agenda medica.

---
+++ SaludGuru.MedicalCalendar/MedicalCalendar.Manager/DAL/Controller/MedicalCalendarDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCalendar.Manager.DAL.Controller
+{
+    internal class MedicalCalendarDataController : MedicalCalendar.Manager.Interfaces.IMedicalCalendarData
+    {
+        private static MedicalCalendar.Manager.Interfaces.IMedicalCalendarData oInstance;
+        internal static MedicalCalendar.Manager.Interfaces.IMedicalCalendarData Instance
+        {
+            get
+            {
+                if (oInstance == null)
+                    oInstance = new MedicalCalendarDataController();
+                return oInstance;
+            }
+        }
+    }
+}
---
+++ SaludGuru.MedicalCalendar/MedicalCalendar.Manager/DAL/MySQLDAO/MedicalCalendar_MySqlDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCalendar.Manager.DAL.MySQLDAO
+{
+    internal class MedicalCalendar_MySqlDao : MedicalCalendar.Manager.Interfaces.IMedicalCalendarData
+    {
+    }
+}
