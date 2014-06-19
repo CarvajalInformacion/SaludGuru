bdac762c846a4c02f357c89e1deb179fc3cf8f85
Davidmt89 <d.alonsomt@gmail.com>
2014-06-19 10:07:04 -0500

    se agregan los metodos para agregar y eliminar especialidades y seguros por profesional

--- SaludGuru.Profile/Profile.Test/ProfileTest.cs
+++ SaludGuru.Profile/Profile.Test/ProfileTest.cs
@@ -8,19 +8,45 @@ namespace Profile.Test
     [TestClass]
     public class ProfileTest
     {
-        [TestMethod]
-        public void ProfileSearch()
-        {
-            SaludGuruProfile.Manager.Controller.Profile profile = new SaludGuruProfile.Manager.Controller.Profile();
-            List<ProfileModel> oReturn = profile.ProfileSearch("da mo", 10, 0);
-        }
+        //[TestMethod]
+        //public void ProfileSearch()
+        //{
+        //    SaludGuruProfile.Manager.Controller.Profile profile = new SaludGuruProfile.Manager.Controller.Profile();
+        //    List<ProfileModel> oReturn = profile.ProfileSearch("da mo", 10, 0);
+        //    Assert.AreEqual(oReturn.Count > 0, true);
+        //}
 
-        [TestMethod]
-        public void GetProfileOptions()
-        {
-            List<SaludGuruProfile.Manager.Models.General.ItemModel> oResult =
-                SaludGuruProfile.Manager.Controller.Profile.GetProfileOptions();
-            Assert.AreEqual(oResult.Count > 0, true);
-        }
+        //[TestMethod]
+        //public void GetProfileOptions()
+        //{
+        //    List<SaludGuruProfile.Manager.Models.General.ItemModel> oResult =
+        //        SaludGuruProfile.Manager.Controller.Profile.GetProfileOptions();
+        //    Assert.AreEqual(oResult.Count > 0, true);
+        //}
+
+        //[TestMethod]
+        //public void SpecialtyProfileUpsert()
+        //{
+        //    SaludGuruProfile.Manager.Controller.Profile.SpecialtyProfileUpsert("4321", 27, false);
+        //}
+
+        //[TestMethod]
+        //public void SpecialtyProfileDelete()
+        //{
+        //    SaludGuruProfile.Manager.Controller.Profile.SpecialtyProfileDelete("4321", 27);
+        //}
+
+        //[TestMethod]
+        //public void InsuranceProfileUpsert()
+        //{
+        //    SaludGuruProfile.Manager.Controller.Profile.InsuranceProfileUpsert("4321", 1, false);
+        //}
+
+        //[TestMethod]
+        //public void InsuranceProfileDelete()
+        //{
+        //    SaludGuruProfile.Manager.Controller.Profile.InsuranceProfileDelete("4321", 1);
+        //}
+
     }
 }
