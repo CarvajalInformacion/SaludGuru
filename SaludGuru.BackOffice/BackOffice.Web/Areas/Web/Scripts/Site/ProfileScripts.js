42edda5c401b75a5ecce66f1bd66414638497ad4
jairoguzman <jairo.guzman@carvajal.com>
2014-06-21 18:59:57 -0500

    estructura de treatment office.

--- SaludGuru.BackOffice/BackOffice.Web/Areas/Web/Scripts/Site/ProfileScripts.js
+++ SaludGuru.BackOffice/BackOffice.Web/Areas/Web/Scripts/Site/ProfileScripts.js
@@ -1,4 +1,5 @@
-﻿function OfficeListGrid(vidDiv, vCreateUrl, vOfficeData) {
+﻿//init office grid
+function OfficeListGrid(vidDiv, vCreateUrl, vOfficeData) {
 
     $('#' + vidDiv).kendoGrid({
         toolbar: [{ template: '<a href="' + vCreateUrl + '">Nueva oficina</a>' }],
@@ -25,5 +26,34 @@
                 title: "Creación"
         }]
     });
+}
 
+//init office treatment grid
+function OfficeTreatmentListGrid(vidDiv, vCreateUrl, vTreatmentData) {
+
+    $('#' + vidDiv).kendoGrid({
+        toolbar: [{ template: '<a href="' + vCreateUrl + '">Nueva oficina</a>' }],
+        dataSource: {
+            type: "json",
+            data: vOfficeData,
+        },
+
+        columns: [{
+            field: "Name",
+            title: "Nombre",
+            template: '<a href="${UrlToUpdate}">${Name}</a>'
+        }, {
+            field: "CityName",
+            title: "Ciudad"
+        }, {
+            field: "IsDefault",
+            title: "Predeterminada"
+        }, {
+            field: "LastModify",
+            title: "Modificación"
+        }, {
+            field: "CreateDate",
+            title: "Creación"
+        }]
+    });
 }
