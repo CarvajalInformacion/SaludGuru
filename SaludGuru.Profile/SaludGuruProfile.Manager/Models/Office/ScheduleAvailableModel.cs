64c32f0815564d540f584a8f13f89369e9bd65c4
jairoguzman <jairo.guzman@carvajal.com>
2014-06-24 08:53:08 -0500

    Agrega nombre del dia de la semana.

--- SaludGuru.Profile/SaludGuruProfile.Manager/Models/Office/ScheduleAvailableModel.cs
+++ SaludGuru.Profile/SaludGuruProfile.Manager/Models/Office/ScheduleAvailableModel.cs
@@ -10,6 +10,38 @@ namespace SaludGuruProfile.Manager.Models.Office
     {
         public int ScheduleAvailableId { get; set; }
         public DayOfWeek Day { get; set; }
+        public string DayName
+        {
+            get
+            {
+                string oReturn = string.Empty;
+                switch (Day)
+                {
+                    case DayOfWeek.Monday:
+                        oReturn = "Lunes";
+                        break;
+                    case DayOfWeek.Tuesday:
+                        oReturn = "Martes";
+                        break;
+                    case DayOfWeek.Wednesday:
+                        oReturn = "Miercoles";
+                        break;
+                    case DayOfWeek.Thursday:
+                        oReturn = "Jueves";
+                        break;
+                    case DayOfWeek.Friday:
+                        oReturn = "Viernes";
+                        break;
+                    case DayOfWeek.Saturday:
+                        oReturn = "Sabado";
+                        break;
+                    case DayOfWeek.Sunday:
+                        oReturn = "Domingo";
+                        break;
+                }
+                return oReturn;
+            }
+        }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
         public DateTime CreateDate { get; set; }
