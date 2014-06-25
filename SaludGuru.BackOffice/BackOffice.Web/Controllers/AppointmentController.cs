14fabeff4d81637b9af5352973ffd6a48995ae55
jairoguzman <jairo.guzman@carvajal.com>
2014-06-25 13:13:45 -0500

    Agrega estructura de agenda

---
+++ SaludGuru.BackOffice/BackOffice.Web/Controllers/AppointmentController.cs
@@ -0,0 +1,17 @@
+﻿using BackOffice.Models.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackOffice.Web.Controllers
+{
+    public partial class AppointmentController : BaseController
+    {
+        public virtual ActionResult Day()
+        {
+            return View();
+        }
+    }
+}
