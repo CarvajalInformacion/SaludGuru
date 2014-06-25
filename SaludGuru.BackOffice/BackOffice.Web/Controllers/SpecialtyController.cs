2ddcb8546c554af6797e69adebc55a227b407ad1
Davidmt89 <d.alonsomt@gmail.com>
2014-06-24 19:12:38 -0500

    Se realiza la admnistración de especialidades

--- SaludGuru.BackOffice/BackOffice.Web/Controllers/SpecialtyController.cs
+++ SaludGuru.BackOffice/BackOffice.Web/Controllers/SpecialtyController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BackOffice.Models.General;
+using BackOffice.Models.Specialty;
+using SaludGuruProfile.Manager.Controller;
+using SaludGuruProfile.Manager.Models;
+using SaludGuruProfile.Manager.Models.General;
+using SaludGuruProfile.Manager.Models.Profile;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +18,27 @@ namespace BackOffice.Web.Controllers
         {
             return View();
         }
+
+        public virtual ActionResult SpecialtyList(string ProfilePublicId)
+        {
+            List<SpecialtyModel> Model = new List<SpecialtyModel>();
+
+            Model = SaludGuruProfile.Manager.Controller.Specialty.GetAllAdmin(ProfilePublicId);
+           
+            return View(Model);
+        }
+
+        public virtual ActionResult SpecialtyUpsert(string SpecialtyPublicId, string ProfilePublicId)
+        {
+            //SpecialtyUpsertModel Model = new SpecialtyUpsertModel()
+            //{
+            //    Profile = SaludGuruProfile.Manager.Controller.Profile.ProfileGetFullAdmin(ProfilePublicId),
+            //    CurrentSpecialty = string.IsNullOrEmpty(SpecialtyPublicId) ? null : SaludGuruProfile.Manager.Controller.Specialty.GetAllAdmin(SpecialtyPublicId),
+            //};
+
+            return View();
+        }
+
+
     }
 }
