637135096b5d6173761526c287e3b629262a309f
Davidmt89 <d.alonsomt@gmail.com>
2014-06-25 18:15:50 -0500

    se modifica la lógica de profile specialty

--- SaludGuru.BackOffice/BackOffice.Web/Controllers/ProfileController.cs
+++ SaludGuru.BackOffice/BackOffice.Web/Controllers/ProfileController.cs
@@ -234,16 +234,6 @@ namespace BackOffice.Web.Controllers
             return View();
         }
 
-        public virtual ActionResult ProfileSpecialtyDelete(string ProfilePublicId)
-        {
-            if(!string.IsNullOrEmpty(Request["UpsertAction"])
-                && bool.Parse(Request["UpsertAction"]))
-            {
-                int profileSpecialtyId = int.Parse(Request["ProfileSpecialtyId"].ToString().Trim());
-                
-            }
-        }
-
         #endregion
 
         #region Insurance
