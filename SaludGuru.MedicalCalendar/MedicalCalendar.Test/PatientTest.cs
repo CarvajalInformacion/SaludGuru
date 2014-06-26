a0565da58f872fff756874aef2138ae265ba81cd
SebasDeveloper <johann.martinez@carvajal.com>
2014-06-26 18:53:43 -0500

    Se implemento la funcionalidad de Get all de Patient

---
+++ SaludGuru.MedicalCalendar/MedicalCalendar.Test/PatientTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MedicalCalendar.Test
+{
+    [TestClass]
+    public class PatientTest
+    {
+        [TestMethod]
+        public void PatientGetAllByPublicPatientId()
+        {
+
+        }
+    }
+}
