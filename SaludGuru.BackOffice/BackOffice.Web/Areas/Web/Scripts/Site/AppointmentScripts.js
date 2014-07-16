f9f802b0e1b7e5a52cedf437d2ea4913d67fb422
Davidmt89 <d.alonsomt@gmail.com>
2014-07-16 11:08:56 -0500

    se realiza modificaciones en la lista de appoitnment

--- SaludGuru.BackOffice/BackOffice.Web/Areas/Web/Scripts/Site/AppointmentScripts.js
+++ SaludGuru.BackOffice/BackOffice.Web/Areas/Web/Scripts/Site/AppointmentScripts.js
@@ -446,16 +446,8 @@ var MeetingListObject = {
             },
             columns: [{
                 field: "title",
-                title: "Nombre",
-            }, {
-                title: "LastName",
-                title: "Apellidos"
-            }, {
-                field: "end",
-                title: "Fecha Fin",
-            }, {
-                field: "start",
-                title: "Fecha Inicio",
+                title: "Citas",
+                template: $('#AppointmentTemplate').html()
             }],
         });
     },
