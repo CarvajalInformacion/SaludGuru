cfd4b9755625e675a187ffa256ba4c9cf61b45b6
jairoguzman <jairo.guzman@carvajal.com>
2014-07-30 12:39:38 -0500

    Actualizacion web api construccion de perfil.

---
+++ SaludGuru.MarketPlace/MarketPlace.Models/Profile/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SaludGuruProfile.Manager.Models.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Models.Profile
+{
+    public class ProfileViewModel
+    {
+        public ProfileModel CurrentProfile { get; set; }
+    }
+}
