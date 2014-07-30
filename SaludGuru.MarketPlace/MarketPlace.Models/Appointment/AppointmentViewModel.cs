9e371d4185cd5871e10c48e5b5f3cf049851b96a
jairoguzman <jairo.guzman@carvajal.com>
2014-07-30 11:39:15 -0500

    Modelo de vista para cita de mp.

---
+++ SaludGuru.MarketPlace/MarketPlace.Models/Appointment/AppointmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SaludGuruProfile.Manager.Models.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Models.Appointment
+{
+    public class AppointmentViewModel
+    {
+        public DateTime CurrentDate { get; set; }
+
+        public ProfileModel CurrentProfile { get; set; }
+    }
+}
