0d3b5503d799fc02b922926628a4ee637181d8c9
SebasDeveloper <johann.martinez@carvajal.com>
2014-08-01 10:02:03 -0500

    CreatePatient

--- SaludGuru.MedicalCalendar/MedicalCalendar.Manager/Controller/Patient.cs
+++ SaludGuru.MedicalCalendar/MedicalCalendar.Manager/Controller/Patient.cs
@@ -105,6 +105,50 @@ namespace MedicalCalendar.Manager.Controller
             return MedicalCalendarDataController.Instance.MPPatientGetByUserPublicId(vUserPublicId);
         }
 
+        public static string MPUpsertPatientInfo(PatientModel PatientToUpsert, string ProfilePublicId, string UserPublicId)
+        {
+            string oPublicPatientId = PatientToUpsert.PatientPublicId;
+            if (string.IsNullOrEmpty(oPublicPatientId))
+            {
+                oPublicPatientId = DAL.Controller.MedicalCalendarDataController.Instance.PatientCreate
+                (PatientToUpsert.Name,
+                PatientToUpsert.LastName,
+                ProfilePublicId,
+                UserPublicId);
+            }
+            else
+            {
+                DAL.Controller.MedicalCalendarDataController.Instance.PatientModify
+               (oPublicPatientId,
+               PatientToUpsert.Name,
+               PatientToUpsert.LastName);
+            }
+
+            PatientToUpsert.PatientInfo.All(pri =>
+            {
+                if (pri.PatientInfoId <= 0)
+                {
+                    //create info
+                    DAL.Controller.MedicalCalendarDataController.Instance.PatientInfoCreate
+                        (oPublicPatientId,
+                        pri.PatientInfoType,
+                        pri.Value,
+                        pri.LargeValue);
+                }
+                else
+                {
+                    //update info
+                    DAL.Controller.MedicalCalendarDataController.Instance.PatientInfoModify
+                        (pri.PatientInfoId,
+                        pri.Value,
+                        pri.LargeValue);
+                }
+
+                return true;
+            });
+            return oPublicPatientId;
+        }
+
         #endregion
     }
 }
