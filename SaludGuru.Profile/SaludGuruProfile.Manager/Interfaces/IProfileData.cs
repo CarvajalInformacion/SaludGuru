f9276c90cf9937132ad785eefcdb013130d21139
jairoguzman <jairo.guzman@carvajal.com>
2014-08-08 18:09:57 -0500

    Agrega metodo de consulta de antiguo id.

--- SaludGuru.Profile/SaludGuruProfile.Manager/Interfaces/IProfileData.cs
+++ SaludGuru.Profile/SaludGuruProfile.Manager/Interfaces/IProfileData.cs
@@ -58,6 +58,7 @@ namespace SaludGuruProfile.Manager.Interfaces
         ProfileModel MPProfileGetFullOfficeCategory(string ProfilePublicId);
         ProfileModel MPProfileGetFullOfficeScheduleAvailable(string ProfilePublicId);
         ProfileModel MPProfileGetFullRelatedProfile(string ProfilePublicId);
+        string MPProfileGetProfilePublicIdFromOldId(string OldProfileId);
 
         #endregion
 
