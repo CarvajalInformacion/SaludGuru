625c913bcebde306a0934032266a33e5c9dc29a2
jairoguzman <jairo.guzman@carvajal.com>
2014-08-12 09:48:02 -0500

    Crea pry de feed de wordpress.

---
+++ SaludGuru.FeedManager/SaludGuru.FeedManager/Readers/FeedReaderFactory.cs
@@ -0,0 +1,27 @@
+﻿using SaludGuru.FeedManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludGuru.FeedManager
+{
+    internal class FeedReaderFactory
+    {
+        public IFeedReader GetFeedReaderRepository(string FeedModule)
+        {
+            //get assembli info
+            string AssemblyInfo = Models.InternalSettings.Instance
+                [Models.Constants.C_Settings_FeedReader_AssemblyInfo.
+                Replace("{{FeedReader}}", FeedModule)].Value.Replace(" ", "");
+
+            Type typetoreturn = Type.GetType(AssemblyInfo);
+            IFeedReader oRetorno = (IFeedReader)Activator.CreateInstance(typetoreturn);
+
+            oRetorno.FeedModule = FeedModule;
+
+            return oRetorno;
+        }
+    }
+}
