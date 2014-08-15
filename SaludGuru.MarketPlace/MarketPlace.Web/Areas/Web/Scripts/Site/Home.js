40702c4ef3ed30f16090f0385ccbad13004aa7df
Davidmt89 <d.alonsomt@gmail.com>
2014-08-15 17:04:01 -0500

    Se modifican estilos de home

---
+++ SaludGuru.MarketPlace/MarketPlace.Web/Areas/Web/Scripts/Site/Home.js
@@ -0,0 +1,20 @@
+﻿/*init function view contact*/
+function fnInitHome(vInitObject) {
+    //init home image
+    var count = vInitObject.CountMax;
+    var img = Math.floor(Math.random() * count) + 1;
+    var imageUrl = vInitObject.Baseurl.replace(/{{Count}}/gi, img);
+    $(document).ready(function () {
+        $('.SearchBox').css('background', 'url("' + imageUrl + '") no-repeat');
+    });
+
+    //target blank articles
+    $('a').each(function () {
+        var a = new RegExp('/' + window.location.host + '/');
+        if (!a.test(this.href)) {
+            $(this).attr("target", "_blank");
+        }
+    });
+
+
+}
