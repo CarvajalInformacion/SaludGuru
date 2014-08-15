f6c0c262642f3f670a825c541e443ac1504962a8
jairoguzman <jairo.guzman@carvajal.com>
2014-08-15 15:46:34 -0500

    Agrega mensaje de correo no autorizado.

--- SaludGuru.BackOffice/BackOffice.Models/General/Constants.cs
+++ SaludGuru.BackOffice/BackOffice.Models/General/Constants.cs
@@ -12,10 +12,18 @@
 
         public const string C_SettingsModuleName = "BackOffice";
 
+        #region ViewData
+
+        public const string C_ViewData_UserNotAutorizedText = "UserNotAutorizedText";
+
+        #endregion
+
         #region LoginModule
 
         public const string C_Settings_Login_FBUrl = "{{AreaName}}_Login_FBUrl";
         public const string C_Settings_Login_GoogleUrl = "{{AreaName}}_Login_GoogleUrl";
+
+        public const string C_Settings_Login_UserNotAutorized = "Login_UserNotAutorized";
 
         #endregion
 
