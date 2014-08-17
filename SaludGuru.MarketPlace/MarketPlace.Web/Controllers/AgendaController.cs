69d3327c70c209e0366c8fca17b9437814b51e7a
jairoguzman <jairo.guzman@carvajal.com>
2014-08-16 19:54:24 -0500

    Crea redireccion de confirmacion de citas

---
+++ SaludGuru.MarketPlace/MarketPlace.Web/Controllers/AgendaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarketPlace.Web.Controllers
+{
+    public partial class AgendaController : BaseController
+    {
+        public virtual ActionResult CancelAppointment(string id)
+        {
+            string NewAppointmentPublicId = MedicalCalendar.Manager.Controller.Appointment.GetAppointmentByOldId(id);
+
+            if (!string.IsNullOrEmpty(NewAppointmentPublicId) && NewAppointmentPublicId.Trim().Length >= 8)
+            {
+                return RedirectPermanent("https://localhost:44304/ExternalAppointment/Index?AppointmentPublicId=" + NewAppointmentPublicId);
+
+            }
+            else
+            {
+                return RedirectPermanent(Server.UrlDecode(Url.RouteUrl(MarketPlace.Models.General.Constants.C_Route_Error_NotFound)));
+            }
+        }
+    }
+}
