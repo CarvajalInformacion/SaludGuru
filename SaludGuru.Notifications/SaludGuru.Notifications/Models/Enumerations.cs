d44fd04faf257fdc06f70df48c8b63e37aed0571
SebasDeveloper <johann.martinez@carvajal.com>
2014-08-26 11:38:47 -0500

    Se modifico una enumeracion

--- SaludGuru.Notifications/SaludGuru.Notifications/Models/Enumerations.cs
+++ SaludGuru.Notifications/SaludGuru.Notifications/Models/Enumerations.cs
@@ -8,8 +8,12 @@
 
     public enum enumNotificationType
     {
-        CitaCreada = 1501,
-        CitaCancelada = 1502,
-        NvoPaciente = 1503
+        CreatedAppointment = 1501,
+        CancelAppointment = 1502,
+        NewPatient = 1503,
+        ConfirmAppointment = 1504,
+        ReminderAppointment = 1505,
+        ReminderNextAppointment = 1506,
+        Survey = 1507,
     }
 }
