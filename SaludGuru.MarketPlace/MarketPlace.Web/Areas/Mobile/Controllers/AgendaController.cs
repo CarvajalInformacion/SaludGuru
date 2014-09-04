78f345c9bc1eab6e059d7168d302db82f887e185
SebasDeveloper <johann.martinez@carvajal.com>
2014-09-04 16:32:35 -0500

    Mobile

---
+++ SaludGuru.MarketPlace/MarketPlace.Web/Areas/Mobile/Controllers/AgendaController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarketPlace.Web.Areas.Mobile.Controllers
+{
+    public partial class AgendaController : MarketPlace.Web.Controllers.AgendaController
+    {
+
+    }
+}
--- SaludGuru.MarketPlace/MarketPlace.Web/Mobile.SharedController.generated.cs
+++ SaludGuru.MarketPlace/MarketPlace.Web/Mobile.SharedController.generated.cs
@@ -36,7 +36,37 @@ namespace T4MVC.Mobile
             public _ViewNamesClass ViewNames { get { return s_ViewNames; } }
             public class _ViewNamesClass
             {
+                public readonly string _AP_Appointment = "_AP_Appointment";
+                public readonly string _AP_ProfileInfo = "_AP_ProfileInfo";
+                public readonly string _L_Footer = "_L_Footer";
+                public readonly string _L_Header = "_L_Header";
+                public readonly string _Layout = "_Layout";
+                public readonly string _P_Appointment = "_P_Appointment";
+                public readonly string _P_BasicInfo = "_P_BasicInfo";
+                public readonly string _P_DetailInfo = "_P_DetailInfo";
+                public readonly string _P_ImageSlider = "_P_ImageSlider";
+                public readonly string _P_Map = "_P_Map";
+                public readonly string _S_Breadcrumb = "_S_Breadcrumb";
+                public readonly string _S_Content_NoFound = "_S_Content_NoFound";
+                public readonly string _S_Profile_Basic = "_S_Profile_Basic";
+                public readonly string _S_Profile_Free = "_S_Profile_Free";
+                public readonly string _S_Profile_Pay = "_S_Profile_Pay";
             }
+            public readonly string _AP_Appointment = "~/Areas/Mobile/Views/Shared/_AP_Appointment.cshtml";
+            public readonly string _AP_ProfileInfo = "~/Areas/Mobile/Views/Shared/_AP_ProfileInfo.cshtml";
+            public readonly string _L_Footer = "~/Areas/Mobile/Views/Shared/_L_Footer.cshtml";
+            public readonly string _L_Header = "~/Areas/Mobile/Views/Shared/_L_Header.cshtml";
+            public readonly string _Layout = "~/Areas/Mobile/Views/Shared/_Layout.cshtml";
+            public readonly string _P_Appointment = "~/Areas/Mobile/Views/Shared/_P_Appointment.cshtml";
+            public readonly string _P_BasicInfo = "~/Areas/Mobile/Views/Shared/_P_BasicInfo.cshtml";
+            public readonly string _P_DetailInfo = "~/Areas/Mobile/Views/Shared/_P_DetailInfo.cshtml";
+            public readonly string _P_ImageSlider = "~/Areas/Mobile/Views/Shared/_P_ImageSlider.cshtml";
+            public readonly string _P_Map = "~/Areas/Mobile/Views/Shared/_P_Map.cshtml";
+            public readonly string _S_Breadcrumb = "~/Areas/Mobile/Views/Shared/_S_Breadcrumb.cshtml";
+            public readonly string _S_Content_NoFound = "~/Areas/Mobile/Views/Shared/_S_Content_NoFound.cshtml";
+            public readonly string _S_Profile_Basic = "~/Areas/Mobile/Views/Shared/_S_Profile_Basic.cshtml";
+            public readonly string _S_Profile_Free = "~/Areas/Mobile/Views/Shared/_S_Profile_Free.cshtml";
+            public readonly string _S_Profile_Pay = "~/Areas/Mobile/Views/Shared/_S_Profile_Pay.cshtml";
         }
     }
 
