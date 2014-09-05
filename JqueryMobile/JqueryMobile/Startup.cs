adc87161bcbfa180a8c10356398a3bee94df99e4
SebasDeveloper <johann.martinez@carvajal.com>
2014-09-05 16:59:07 -0500

    Merge

---
+++ JqueryMobile/JqueryMobile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JqueryMobile.Startup))]
+namespace JqueryMobile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
