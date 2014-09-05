40bea0633d6e9cd2fcfc26f2940586fa91cf6446
SebasDeveloper <johann.martinez@carvajal.com>
2014-09-05 13:12:59 -0500

    Mobile

---
+++ SaludGuru.MarketPlace/MarketPlace.Web/Areas/Mobile/Scripts/Site/General.js
@@ -0,0 +1,31 @@
+﻿function InitGlobalPagesControlsMobile(InitParams) {
+    debugger;
+    $('#' + InitParams.MenuButton).click(function () {
+        $('#' + InitParams.DivEmergentMenu).show();
+
+    });
+    //init search box 
+    //SearchBoxObject.Init({
+    //    InputId: InitParams.SearchBoxId,
+    //    CityId: InitParams.CityId,
+    //});
+    //SearchBoxObject.RenderAsync();
+
+
+    ////init schedule appointment object
+    //ScheduleAppointmentObject.Init({
+    //    FBUrl: InitParams.FBUrl,
+    //    GoogleUrl: InitParams.GoogleUrl,
+    //    OutlookUrl: InitParams.OutlookUrl,
+    //    IsLogin: InitParams.IsLogin,
+    //});
+
+    //init change city action
+    if ($('#' + InitParams.selCityId).length > 0) {
+        $('#' + InitParams.selCityId).change(function () {
+            if (InitParams.CityId != $(this).val()) {
+                window.location = '/Home/ChangeCity?NewCityId=' + $(this).val();
+            }
+        });
+    }
+}
