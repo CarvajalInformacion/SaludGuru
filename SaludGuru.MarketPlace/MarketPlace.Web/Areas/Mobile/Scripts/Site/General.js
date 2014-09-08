6619b983c7ae399d5d56d1b755c5f49f404ba106
SebasDeveloper <johann.martinez@carvajal.com>
2014-09-08 18:15:56 -0500

    Header Mobile

--- SaludGuru.MarketPlace/MarketPlace.Web/Areas/Mobile/Scripts/Site/General.js
+++ SaludGuru.MarketPlace/MarketPlace.Web/Areas/Mobile/Scripts/Site/General.js
@@ -1,25 +1,25 @@
 ﻿function InitGlobalPagesControlsMobile(InitParams) {
-    debugger;
-    $('#' + InitParams.MenuButton).click(function () {
-        debugger;
-        $('#' + InitParams.DivEmergentMenu).show();
+    //debugger;
+    //$('#' + InitParams.MenuButton).click(function () {
+    //    debugger;
+    //    $('#' + InitParams.DivEmergentMenu).show();
 
-    });
+    //});
     //init search box 
-    //SearchBoxObject.Init({
-    //    InputId: InitParams.SearchBoxId,
-    //    CityId: InitParams.CityId,
-    //});
-    //SearchBoxObject.RenderAsync();
+    SearchBoxObject.Init({
+        InputId: InitParams.SearchBoxId,
+        CityId: InitParams.CityId,
+    });
+    SearchBoxObject.RenderAsync();
 
 
-    ////init schedule appointment object
-    //ScheduleAppointmentObject.Init({
-    //    FBUrl: InitParams.FBUrl,
-    //    GoogleUrl: InitParams.GoogleUrl,
-    //    OutlookUrl: InitParams.OutlookUrl,
-    //    IsLogin: InitParams.IsLogin,
-    //});
+    //init schedule appointment object
+    ScheduleAppointmentObject.Init({
+        FBUrl: InitParams.FBUrl,
+        GoogleUrl: InitParams.GoogleUrl,
+        OutlookUrl: InitParams.OutlookUrl,
+        IsLogin: InitParams.IsLogin,
+    });
 
     //init change city action
     if ($('#' + InitParams.selCityId).length > 0) {
