dab5ec6968bd689cc85dd67c6a4534d09c27e0b8
SebasDeveloper <johann.martinez@carvajal.com>
2014-09-09 09:51:41 -0500

    >Home

--- SaludGuru.MarketPlace/MarketPlace.Web/Areas/Mobile/Scripts/Site/General.js
+++ SaludGuru.MarketPlace/MarketPlace.Web/Areas/Mobile/Scripts/Site/General.js
@@ -30,3 +30,74 @@
         });
     }
 }
+
+
+/*Searchbox objext*/
+var SearchBoxObject = {
+
+    /*profile info*/
+    InputId: '',
+    CityId: '1',
+
+    /*init meeting calendar variables*/
+    Init: function (vInitObject) {
+        this.InputId = vInitObject.InputId;
+        this.CityId = vInitObject.CityId;
+    },
+
+    RenderAsync: function () {
+
+        if ($('#' + SearchBoxObject.InputId).length > 0) {
+            //init on enter event
+            $('#' + SearchBoxObject.InputId).keyup(function (e) {
+                if (e.keyCode == 13) {
+                    SearchBoxObject.SearchTerm();
+                }
+            });
+
+            //init autocomplete
+            $('#' + SearchBoxObject.InputId).autocomplete({
+                source: function (request, response) {
+                    $.ajax({
+                        url: '/api/SearchApi?IsAc=true&CityId=' + SearchBoxObject.CityId + '&SearchParam=' + request.term,
+                        dataType: 'json',
+                        type: "POST",
+                        success: function (data) {
+                            response(data);
+                        }
+                    });
+                },
+                minLength: 2,
+                focus: function (event, ui) {
+                    return false;
+                },
+                select: function (event, ui) {
+                    if (ui.item != null && ui.item.CurrentAcItem != null) {
+                        $('#' + SearchBoxObject.InputId).val(ui.item.CurrentAcItem.Node);
+                        SearchBoxObject.SearchTerm();
+                    }
+                    return false;
+                },
+                messages: {
+                    noResults: '',
+                    results: '',
+                }
+            }).data("ui-autocomplete")._renderItem = function (ul, item) {
+
+                var RenderItem = $('#' + SearchBoxObject.InputId + '_AcTemplate').html();
+                RenderItem = RenderItem.replace(/\${Type}/gi, item.Type);
+                RenderItem = RenderItem.replace(/\${Node}/gi, item.NodeSelected);
+
+                return $("<li></li>")
+                    .data("ui-autocomplete-item", item)
+                    .append("<a>" + RenderItem + "</a>")
+                    .appendTo(ul);
+            };
+        }
+    },
+
+    SearchTerm: function () {
+        //find url to redirect
+        window.location = '/doctores-' + $("#selGlobalCity option:selected").attr('itemname') + '/' + encodeURIComponent($('#' + SearchBoxObject.InputId).val().replace(/\./gi, ''));
+    }
+};
