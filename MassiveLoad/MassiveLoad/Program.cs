c59493f271aaecfe42902417fdb46f7d57d5397a
Davidmt89 <d.alonsomt@gmail.com>
2014-09-10 14:24:22 -0500

    se realizan cambios en los estilos de la version mobile de market place.

--- MassiveLoad/MassiveLoad/Program.cs
+++ MassiveLoad/MassiveLoad/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,8 @@ namespace MassiveLoad
         {
             Console.WriteLine("Cargue masivo de Profesionales");
             ImportData impD = new ImportData();
-            string pathFile = "" + @"D:\Cargue_Masivo\BD_Ibague_y_Medellin.xlsx";
-            string logFile = "" + @"D:\Cargue_Masivo\LogCargueMasivo.txt";
+            string pathFile = ConfigurationManager.AppSettings["ExcelFile"];
+            string logFile = ConfigurationManager.AppSettings["LogFile"];
             Console.WriteLine("Cargando archivo: " + pathFile);
             impD.processFile(pathFile, logFile);
             #region Insert Log Message
