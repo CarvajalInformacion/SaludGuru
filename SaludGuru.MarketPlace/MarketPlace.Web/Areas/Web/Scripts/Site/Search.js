105d4a52c42e979a2dd659ab21d3d9b00617f6af
jairoguzman <jairo.guzman@carvajal.com>
2014-09-18 16:34:30 -0500

    Implementación filtros.

--- SaludGuru.MarketPlace/MarketPlace.Web/Areas/Web/Scripts/Site/Search.js
+++ SaludGuru.MarketPlace/MarketPlace.Web/Areas/Web/Scripts/Site/Search.js
@@ -6,6 +6,25 @@ function fnInitContactOffice() {
     });
 }
 
+/*filter functions*/
+function fnViewMoreLessFilter(vSelector) {
+    if ($('#' + vSelector + '_a').length > 0) {
+
+        if ($('#' + vSelector + '_a').text().trim() == 'Ver más') {
+            $('.' + vSelector).fadeIn('slow');
+            $('#' + vSelector + '_a').text('Ver menos');
+        }
+        else {
+            $('.' + vSelector).fadeOut('slow');
+            $('#' + vSelector + '_a').text('Ver más');
+        }
+    }
+}
+
+function fnFilter(vFilterUrl) {
+    window.location = vFilterUrl;
+}
+
 
 
 
